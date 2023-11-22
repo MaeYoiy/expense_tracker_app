6d25cbd60147d131f612c98a968b93f706057a98
MaeYoiy <b6333034@gmail.com>
2023-11-22 23:21:33 +0700

    Update expense.dart

--- expense_tracker/lib/models/expense.dart
+++ expense_tracker/lib/models/expense.dart
@@ -1,10 +1,14 @@
+import 'package:uuid/uuid.dart';
+
+const uuid = Uuid();
+
 // แบบจำลองหรือพิมพ์เขียวสำหรับ Data
 class Expense {
-  const Expense({
+  Expense({
     required this.title,
     required this.amount,
     required this.date,
-  });
+  }) : id = uuid.v4();
 
   final String id;
   final String title;
