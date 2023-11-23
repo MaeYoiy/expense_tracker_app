143fac55f689c6911bd159b8f421fcd3773cdf8b
MaeYoiy <b6333034@gmail.com>
2023-11-23 22:56:04 +0700

    create expenses_list.dart

--- expense_tracker/lib/expenses.dart
+++ expense_tracker/lib/expenses.dart
@@ -1,3 +1,5 @@
+import 'package:expense_tracker/expenses_list.dart';
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 // Widget stateful ต้องมี 2 class เสมอ คือ Expenses และ _ExpensesState
@@ -13,13 +15,30 @@ class Expenses extends StatefulWidget {
 
 // State<Expenses> State เชื่อมต่อกับ Expenses
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 15.69,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
-          Text('The chart'),
-          Text('Expense List...'),
+          const Text('The chart'),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          ),
         ],
       ),
     );
--- expense_tracker/lib/models/expense.dart
+++ expense_tracker/lib/models/expense.dart
@@ -2,16 +2,21 @@ import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
+// enum = เป็น keyword ที่ช่วยให้เราสร้างประเภทที่กำหนดเองได้
+enum Category { food, travel, leisure, work }
+
 // แบบจำลองหรือพิมพ์เขียวสำหรับ Data
 class Expense {
   Expense({
     required this.title,
     required this.amount,
     required this.date,
+    required this.category,
   }) : id = uuid.v4();
 
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 }
