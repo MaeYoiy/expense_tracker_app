8815fe729530cda6a69214b058faaff182995e96
MaeYoiy <b6333034@gmail.com>
2023-12-04 01:12:14 +0700

    Updated expenses.dart

--- expense_tracker/lib/widgets/new_expense.dart
+++ expense_tracker/lib/widgets/new_expense.dart
@@ -1,4 +1,7 @@
 // import 'package:flutter/foundation.dart';
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
 
@@ -34,17 +37,25 @@ class _NewExpenseState extends State<NewExpense> {
     });
   }
 
-  void _submitExpenseData() {
-    // tryParse('Hello') -> null, tryParse('1.12') -> 1.12
-    // แปลง ตัวเลขที่เป็น String ให้เป็น double
-    final enteredAmount = double.tryParse(_amountController.text);
-    final amountIsInvalid = enteredAmount == null || enteredAmount <= 0;
-
-    // if (ถ้าไม่ได้กรอกข้อมูล || amount ไม่ได้กรอกเข้ามา,ไม่ได้กรอกตัวเลขแต่กรอก alphabet, มา น้อยกว่าหรือเท่ากับ 0 || ไม่ได้เลือกวันที่)
-    // _titleController.text.trim().isEmpty -> .text = ดึงข้อความจากคอนโทรลเลอร์, .text = ลบช่องว่างที่อยู่ด้านหน้าหรือด้านหลังของข้อความ, .isEmpty = ข้อความที่ได้ว่างเปล่าหรือไม่
-    if (_titleController.text.trim().isEmpty ||
-        amountIsInvalid ||
-        _selectedDate == null) {
+  void _showDialog() {
+    if (Platform.isIOS) {
+      showCupertinoDialog(
+          context: context,
+          builder: (ctx) => CupertinoAlertDialog(
+                title: const Text('Invalid input'),
+                content: const Text(
+                    'Please make sure a valid title, amount, date and category was entered.'),
+                // actions อย่าง ผู้ใช้กด Okay ออกจาก Alert
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(ctx);
+                    },
+                    child: const Text('Okay'),
+                  ),
+                ],
+              ));
+    } else {
       showDialog(
         context: context,
         builder: (ctx) => AlertDialog(
@@ -62,6 +73,21 @@ class _NewExpenseState extends State<NewExpense> {
           ],
         ),
       );
+    }
+  }
+
+  void _submitExpenseData() {
+    // tryParse('Hello') -> null, tryParse('1.12') -> 1.12
+    // แปลง ตัวเลขที่เป็น String ให้เป็น double
+    final enteredAmount = double.tryParse(_amountController.text);
+    final amountIsInvalid = enteredAmount == null || enteredAmount <= 0;
+
+    // if (ถ้าไม่ได้กรอกข้อมูล || amount ไม่ได้กรอกเข้ามา,ไม่ได้กรอกตัวเลขแต่กรอก alphabet, มา น้อยกว่าหรือเท่ากับ 0 || ไม่ได้เลือกวันที่)
+    // _titleController.text.trim().isEmpty -> .text = ดึงข้อความจากคอนโทรลเลอร์, .text = ลบช่องว่างที่อยู่ด้านหน้าหรือด้านหลังของข้อความ, .isEmpty = ข้อความที่ได้ว่างเปล่าหรือไม่
+    if (_titleController.text.trim().isEmpty ||
+        amountIsInvalid ||
+        _selectedDate == null) {
+      _showDialog();
       return;
     }
 
@@ -93,106 +119,217 @@ class _NewExpenseState extends State<NewExpense> {
   @override
   Widget build(BuildContext context) {
     final keyboardSpace = MediaQuery.of(context).viewInsets.bottom;
-    return SizedBox(
-      height: double.infinity,
-      child: SingleChildScrollView(
-        child: Padding(
-          // EdgeInsets.fromLTRB(16, 48, 16, 16) ยิ่งเลขเยอะยิ่งห่าง
-          padding: EdgeInsets.fromLTRB(16, 48, 16, keyboardSpace + 16),
-          child: Column(
-            children: [
-              // column 1
-              // title
-              TextField(
-                // onChanged: _saveTitleInput,
-                controller: _titleController,
-                maxLength: 50,
-                decoration: const InputDecoration(
-                  label: Text('Title'),
-                ),
-              ),
-              // column 2
-              Row(
-                children: [
-                  // amount
-                  Expanded(
-                    child: TextField(
-                      controller: _amountController,
-                      keyboardType: TextInputType.number,
-                      decoration: const InputDecoration(
-                        prefixText: '\$ ',
-                        label: Text('Amount'),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(width: 16),
-                  Expanded(
-                    child: Row(
-                      // ผลักไปทางขวาสุด
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      // ตรงกลางใน row
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        // "!" -> เป็นการบอกบังคับว่า _selectedDate จะไม่เป็นค่าว่าง
-                        Text(
-                          _selectedDate == null
-                              ? 'No date selected'
-                              : formatter.format(_selectedDate!),
-                        ),
-                        IconButton(
-                          onPressed: _presentDatePicker,
-                          icon: const Icon(Icons.calendar_month),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-
-              const SizedBox(height: 16),
-              // column 3
-              Row(
-                children: [
-                  DropdownButton(
-                    value: _selectedCategory,
-                    items: Category.values
-                        .map(
-                          (e) => DropdownMenuItem(
-                            value: e,
-                            child: Text(
-                              e.name.toUpperCase(),
-                            ),
+    return LayoutBuilder(builder: (ctx, constraints) {
+      final width = constraints.maxWidth;
+      return SizedBox(
+        height: double.infinity,
+        child: SingleChildScrollView(
+          child: Padding(
+            // EdgeInsets.fromLTRB(16, 48, 16, 16) ยิ่งเลขเยอะยิ่งห่าง
+            padding: EdgeInsets.fromLTRB(16, 48, 16, keyboardSpace + 16),
+            child: Column(
+              children: [
+                // width >= 600 คือแนวนอน
+                if (width >= 600)
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // Row 1
+                      // title
+                      Expanded(
+                        child: TextField(
+                          // onChanged: _saveTitleInput,
+                          controller: _titleController,
+                          maxLength: 50,
+                          decoration: const InputDecoration(
+                            label: Text('Title'),
                           ),
-                        )
-                        .toList(),
-                    onChanged: (value) {
-                      if (value == null) {
-                        return;
-                      }
-                      setState(
-                        () {
-                          _selectedCategory = value;
+                        ),
+                      ),
+                      const SizedBox(width: 24),
+                      // Row 2
+                      // amount
+                      Expanded(
+                        child: TextField(
+                          controller: _amountController,
+                          keyboardType: TextInputType.number,
+                          decoration: const InputDecoration(
+                            prefixText: '\$ ',
+                            label: Text('Amount'),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                // แนวตั้ง
+                else
+                  // column 1
+                  // title
+                  TextField(
+                    // onChanged: _saveTitleInput,
+                    controller: _titleController,
+                    maxLength: 50,
+                    decoration: const InputDecoration(
+                      label: Text('Title'),
+                    ),
+                  ),
+
+                // width >= 600 คือแนวนอน
+                if (width >= 600)
+                  Row(
+                    children: [
+                      // Column 2 Row 1
+                      DropdownButton(
+                        value: _selectedCategory,
+                        items: Category.values
+                            .map(
+                              (e) => DropdownMenuItem(
+                                value: e,
+                                child: Text(
+                                  e.name.toUpperCase(),
+                                ),
+                              ),
+                            )
+                            .toList(),
+                        onChanged: (value) {
+                          if (value == null) {
+                            return;
+                          }
+                          setState(
+                            () {
+                              _selectedCategory = value;
+                            },
+                          );
                         },
-                      );
-                    },
+                      ),
+                      const SizedBox(width: 24),
+                      // Column 2 Row 2
+                      Expanded(
+                        child: Row(
+                          // ผลักไปทางขวาสุด
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          // ตรงกลางใน row
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            // "!" -> เป็นการบอกบังคับว่า _selectedDate จะไม่เป็นค่าว่าง
+                            Text(
+                              _selectedDate == null
+                                  ? 'No date selected'
+                                  : formatter.format(_selectedDate!),
+                            ),
+                            IconButton(
+                              onPressed: _presentDatePicker,
+                              icon: const Icon(Icons.calendar_month),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  )
+                // แนวตั้ง
+                else
+                  // column 2
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          controller: _amountController,
+                          keyboardType: TextInputType.number,
+                          decoration: const InputDecoration(
+                            prefixText: '\$ ',
+                            label: Text('Amount'),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 16),
+                      Expanded(
+                        child: Row(
+                          // ผลักไปทางขวาสุด
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          // ตรงกลางใน row
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            // "!" -> เป็นการบอกบังคับว่า _selectedDate จะไม่เป็นค่าว่าง
+                            Text(
+                              _selectedDate == null
+                                  ? 'No date selected'
+                                  : formatter.format(_selectedDate!),
+                            ),
+                            IconButton(
+                              onPressed: _presentDatePicker,
+                              icon: const Icon(Icons.calendar_month),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
-                  const Spacer(),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: const Text('Cancle'),
+                const SizedBox(height: 16),
+
+                // width >= 600 คือแนวนอน
+                if (width >= 600)
+                  Row(
+                    children: [
+                      // Column 3 Row 1
+                      const Spacer(),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Text('Cancle'),
+                      ),
+                      ElevatedButton(
+                        onPressed: _submitExpenseData,
+                        child: const Text('Save expense'),
+                      ),
+                    ],
+                  )
+                // แนวตั้ง
+                else
+                  // column 3
+                  Row(
+                    children: [
+                      DropdownButton(
+                        value: _selectedCategory,
+                        items: Category.values
+                            .map(
+                              (e) => DropdownMenuItem(
+                                value: e,
+                                child: Text(
+                                  e.name.toUpperCase(),
+                                ),
+                              ),
+                            )
+                            .toList(),
+                        onChanged: (value) {
+                          if (value == null) {
+                            return;
+                          }
+                          setState(
+                            () {
+                              _selectedCategory = value;
+                            },
+                          );
+                        },
+                      ),
+                      const Spacer(),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Text('Cancle'),
+                      ),
+                      ElevatedButton(
+                        onPressed: _submitExpenseData,
+                        child: const Text('Save expense'),
+                      ),
+                    ],
                   ),
-                  ElevatedButton(
-                    onPressed: _submitExpenseData,
-                    child: const Text('Save expense'),
-                  ),
-                ],
-              ),
-            ],
+              ],
+            ),
           ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
